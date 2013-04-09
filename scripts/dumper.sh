bd169966345ac547926c03cdc04f2d8c2128905a
Greg Robbins <greg.pwpp@gmail.com>
2013-04-09 14:33:30 +0200

    cleaned dumper script and added customer data to be not dumped

--- scripts/dumper.sh
+++ scripts/dumper.sh
@@ -2,8 +2,6 @@
 ### <password><![CDATA[r8R7Q7mBgeYS]]></password>
 ### <dbname><![CDATA[fleshlig_magento]]></dbname>
 
-mysqldump -ufleshlig_magento -pr8R7Q7mBgeYS fleshlig_magento --no-data > fli.magento.dev.dump.sql
-mysqldump -ufleshlig_magento -pr8R7Q7mBgeYS fleshlig_magento -t  \
 # mysql> show tables where Tables_in_fleshlig_magento like '%magento_log%';
 # +----------------------------+
 # | Tables_in_fleshlig_magento |
@@ -19,15 +17,6 @@ mysqldump -ufleshlig_magento -pr8R7Q7mBgeYS fleshlig_magento -t  \
 # | magento_log_visitor_online |
 # +----------------------------+
 # 9 rows in set (0.00 sec)
-    --ignore-table fleshlig_magento.magento_log_customer \
-    --ignore-table fleshlig_magento.magento_log_quote \
-    --ignore-table fleshlig_magento.magento_log_summary \
-    --ignore-table fleshlig_magento.magento_log_summary_type \
-    --ignore-table fleshlig_magento.magento_log_url \
-    --ignore-table fleshlig_magento.magento_log_url_info \
-    --ignore-table fleshlig_magento.magento_log_visitor \
-    --ignore-table fleshlig_magento.magento_log_visitor_info \
-    --ignore-table fleshlig_magento.magento_log_visitor_online \
 # mysql> show tables where Tables_in_fleshlig_magento like '%session%';
 # +----------------------------+
 # | Tables_in_fleshlig_magento |
@@ -38,33 +27,6 @@ mysqldump -ufleshlig_magento -pr8R7Q7mBgeYS fleshlig_magento -t  \
 # | magento_persistent_session |
 # +----------------------------+
 # 4 rows in set (0.00 sec)
-    --ignore-table fleshlig_magento.magento_api_session \
-    --ignore-table fleshlig_magento.magento_core_session \
-    --ignore-table fleshlig_magento.magento_dataflow_session \
-    --ignore-table fleshlig_magento.magento_persistent_session \
-# mysql> show tables where Tables_in_fleshlig_magento like '%enterprise_customer%';
-# +----------------------------------------------+
-# | Tables_in_fleshlig_magento                   |
-# +----------------------------------------------+
-# | enterprise_customer_sales_flat_order         |
-# | enterprise_customer_sales_flat_order_address |
-# | enterprise_customer_sales_flat_quote         |
-# | enterprise_customer_sales_flat_quote_address |
-# | enterprise_customerbalance                   |
-# | enterprise_customerbalance_history           |
-# | enterprise_customersegment_customer          |
-# | enterprise_customersegment_event             | KEEP
-# | enterprise_customersegment_segment           | KEEP
-# | enterprise_customersegment_website           | KEEP
-# +----------------------------------------------+
-# 10 rows in set (0.00 sec)
-    --ignore-table fleshlig_magento.enterprise_customer_sales_flat_order \
-    --ignore-table fleshlig_magento.enterprise_customer_sales_flat_order_address \
-    --ignore-table fleshlig_magento.enterprise_customer_sales_flat_quote \
-    --ignore-table fleshlig_magento.enterprise_customer_sales_flat_quote_address \
-    --ignore-table fleshlig_magento.enterprise_customerbalance \
-    --ignore-table fleshlig_magento.enterprise_customerbalance_history \
-    --ignore-table fleshlig_magento.enterprise_customersegment_customer \
 # mysql> show tables where Tables_in_fleshlig_magento like 'sales_%';
 # +--------------------------------------+
 # | Tables_in_fleshlig_magento           |
@@ -126,61 +88,6 @@ mysqldump -ufleshlig_magento -pr8R7Q7mBgeYS fleshlig_magento -t  \
 # | salesrule_website                    |
 # +--------------------------------------+
 # 55 rows in set (0.00 sec)
-    --ignore-table fleshlig_magento.sales_bestsellers_aggregated_daily \
-    --ignore-table fleshlig_magento.sales_bestsellers_aggregated_monthly \
-    --ignore-table fleshlig_magento.sales_bestsellers_aggregated_yearly \
-    --ignore-table fleshlig_magento.sales_billing_agreement \
-    --ignore-table fleshlig_magento.sales_billing_agreement_order \
-    --ignore-table fleshlig_magento.sales_flat_creditmemo \
-    --ignore-table fleshlig_magento.sales_flat_creditmemo_comment \
-    --ignore-table fleshlig_magento.sales_flat_creditmemo_grid \
-    --ignore-table fleshlig_magento.sales_flat_creditmemo_item \
-    --ignore-table fleshlig_magento.sales_flat_invoice \
-    --ignore-table fleshlig_magento.sales_flat_invoice_comment \
-    --ignore-table fleshlig_magento.sales_flat_invoice_grid \
-    --ignore-table fleshlig_magento.sales_flat_invoice_item \
-    --ignore-table fleshlig_magento.sales_flat_order \
-    --ignore-table fleshlig_magento.sales_flat_order_address \
-    --ignore-table fleshlig_magento.sales_flat_order_grid \
-    --ignore-table fleshlig_magento.sales_flat_order_item \
-    --ignore-table fleshlig_magento.sales_flat_order_payment \
-    --ignore-table fleshlig_magento.sales_flat_order_status_history \
-    --ignore-table fleshlig_magento.sales_flat_quote \
-    --ignore-table fleshlig_magento.sales_flat_quote_address \
-    --ignore-table fleshlig_magento.sales_flat_quote_address_item \
-    --ignore-table fleshlig_magento.sales_flat_quote_item \
-    --ignore-table fleshlig_magento.sales_flat_quote_item_option \
-    --ignore-table fleshlig_magento.sales_flat_quote_payment \
-    --ignore-table fleshlig_magento.sales_flat_quote_shipping_rate \
-    --ignore-table fleshlig_magento.sales_flat_shipment \
-    --ignore-table fleshlig_magento.sales_flat_shipment_comment \
-    --ignore-table fleshlig_magento.sales_flat_shipment_grid \
-    --ignore-table fleshlig_magento.sales_flat_shipment_item \
-    --ignore-table fleshlig_magento.sales_flat_shipment_track \
-    --ignore-table fleshlig_magento.sales_invoiced_aggregated \
-    --ignore-table fleshlig_magento.sales_invoiced_aggregated_order \
-    --ignore-table fleshlig_magento.sales_order_aggregated_created \
-    --ignore-table fleshlig_magento.sales_order_aggregated_updated \
-    --ignore-table fleshlig_magento.sales_order_status \
-    --ignore-table fleshlig_magento.sales_order_status_label \
-    --ignore-table fleshlig_magento.sales_order_status_state \
-    --ignore-table fleshlig_magento.sales_order_tax \
-    --ignore-table fleshlig_magento.sales_order_tax_item \
-    --ignore-table fleshlig_magento.sales_payment_transaction \
-    --ignore-table fleshlig_magento.sales_recurring_profile \
-    --ignore-table fleshlig_magento.sales_recurring_profile_order \
-    --ignore-table fleshlig_magento.sales_refunded_aggregated \
-    --ignore-table fleshlig_magento.sales_refunded_aggregated_order \
-    --ignore-table fleshlig_magento.sales_shipping_aggregated \
-    --ignore-table fleshlig_magento.sales_shipping_aggregated_order \
-    --ignore-table fleshlig_magento.salesrule \
-    --ignore-table fleshlig_magento.salesrule_coupon \
-    --ignore-table fleshlig_magento.salesrule_coupon_usage \
-    --ignore-table fleshlig_magento.salesrule_customer \
-    --ignore-table fleshlig_magento.salesrule_customer_group \
-    --ignore-table fleshlig_magento.salesrule_label \
-    --ignore-table fleshlig_magento.salesrule_product_attribute \
-    --ignore-table fleshlig_magento.salesrule_website \
 # mysql> show tables where Tables_in_fleshlig_magento like 's\_%';
 # +----------------------------------------+
 # | Tables_in_fleshlig_magento             |
@@ -264,6 +171,84 @@ mysqldump -ufleshlig_magento -pr8R7Q7mBgeYS fleshlig_magento -t  \
 # | s_wishlist_item_option                 |
 # +----------------------------------------+
 # 77 rows in set (0.01 sec)
+
+mysqldump -ufleshlig_magento -pr8R7Q7mBgeYS fleshlig_magento --no-data > fli.magento.dev.dump.sql
+mysqldump -ufleshlig_magento -pr8R7Q7mBgeYS fleshlig_magento -t  \
+    --ignore-table fleshlig_magento.magento_log_customer \
+    --ignore-table fleshlig_magento.magento_log_quote \
+    --ignore-table fleshlig_magento.magento_log_summary \
+    --ignore-table fleshlig_magento.magento_log_summary_type \
+    --ignore-table fleshlig_magento.magento_log_url \
+    --ignore-table fleshlig_magento.magento_log_url_info \
+    --ignore-table fleshlig_magento.magento_log_visitor \
+    --ignore-table fleshlig_magento.magento_log_visitor_info \
+    --ignore-table fleshlig_magento.magento_log_visitor_online \
+    --ignore-table fleshlig_magento.magento_api_session \
+    --ignore-table fleshlig_magento.magento_core_session \
+    --ignore-table fleshlig_magento.magento_dataflow_session \
+    --ignore-table fleshlig_magento.magento_persistent_session \
+    --ignore-table fleshlig_magento.enterprise_customer_sales_flat_order \
+    --ignore-table fleshlig_magento.enterprise_customer_sales_flat_order_address \
+    --ignore-table fleshlig_magento.enterprise_customer_sales_flat_quote \
+    --ignore-table fleshlig_magento.enterprise_customer_sales_flat_quote_address \
+    --ignore-table fleshlig_magento.enterprise_customerbalance \
+    --ignore-table fleshlig_magento.enterprise_customerbalance_history \
+    --ignore-table fleshlig_magento.enterprise_customersegment_customer \
+    --ignore-table fleshlig_magento.sales_bestsellers_aggregated_daily \
+    --ignore-table fleshlig_magento.sales_bestsellers_aggregated_monthly \
+    --ignore-table fleshlig_magento.sales_bestsellers_aggregated_yearly \
+    --ignore-table fleshlig_magento.sales_billing_agreement \
+    --ignore-table fleshlig_magento.sales_billing_agreement_order \
+    --ignore-table fleshlig_magento.sales_flat_creditmemo \
+    --ignore-table fleshlig_magento.sales_flat_creditmemo_comment \
+    --ignore-table fleshlig_magento.sales_flat_creditmemo_grid \
+    --ignore-table fleshlig_magento.sales_flat_creditmemo_item \
+    --ignore-table fleshlig_magento.sales_flat_invoice \
+    --ignore-table fleshlig_magento.sales_flat_invoice_comment \
+    --ignore-table fleshlig_magento.sales_flat_invoice_grid \
+    --ignore-table fleshlig_magento.sales_flat_invoice_item \
+    --ignore-table fleshlig_magento.sales_flat_order \
+    --ignore-table fleshlig_magento.sales_flat_order_address \
+    --ignore-table fleshlig_magento.sales_flat_order_grid \
+    --ignore-table fleshlig_magento.sales_flat_order_item \
+    --ignore-table fleshlig_magento.sales_flat_order_payment \
+    --ignore-table fleshlig_magento.sales_flat_order_status_history \
+    --ignore-table fleshlig_magento.sales_flat_quote \
+    --ignore-table fleshlig_magento.sales_flat_quote_address \
+    --ignore-table fleshlig_magento.sales_flat_quote_address_item \
+    --ignore-table fleshlig_magento.sales_flat_quote_item \
+    --ignore-table fleshlig_magento.sales_flat_quote_item_option \
+    --ignore-table fleshlig_magento.sales_flat_quote_payment \
+    --ignore-table fleshlig_magento.sales_flat_quote_shipping_rate \
+    --ignore-table fleshlig_magento.sales_flat_shipment \
+    --ignore-table fleshlig_magento.sales_flat_shipment_comment \
+    --ignore-table fleshlig_magento.sales_flat_shipment_grid \
+    --ignore-table fleshlig_magento.sales_flat_shipment_item \
+    --ignore-table fleshlig_magento.sales_flat_shipment_track \
+    --ignore-table fleshlig_magento.sales_invoiced_aggregated \
+    --ignore-table fleshlig_magento.sales_invoiced_aggregated_order \
+    --ignore-table fleshlig_magento.sales_order_aggregated_created \
+    --ignore-table fleshlig_magento.sales_order_aggregated_updated \
+    --ignore-table fleshlig_magento.sales_order_status \
+    --ignore-table fleshlig_magento.sales_order_status_label \
+    --ignore-table fleshlig_magento.sales_order_status_state \
+    --ignore-table fleshlig_magento.sales_order_tax \
+    --ignore-table fleshlig_magento.sales_order_tax_item \
+    --ignore-table fleshlig_magento.sales_payment_transaction \
+    --ignore-table fleshlig_magento.sales_recurring_profile \
+    --ignore-table fleshlig_magento.sales_recurring_profile_order \
+    --ignore-table fleshlig_magento.sales_refunded_aggregated \
+    --ignore-table fleshlig_magento.sales_refunded_aggregated_order \
+    --ignore-table fleshlig_magento.sales_shipping_aggregated \
+    --ignore-table fleshlig_magento.sales_shipping_aggregated_order \
+    --ignore-table fleshlig_magento.salesrule \
+    --ignore-table fleshlig_magento.salesrule_coupon \
+    --ignore-table fleshlig_magento.salesrule_coupon_usage \
+    --ignore-table fleshlig_magento.salesrule_customer \
+    --ignore-table fleshlig_magento.salesrule_customer_group \
+    --ignore-table fleshlig_magento.salesrule_label \
+    --ignore-table fleshlig_magento.salesrule_product_attribute \
+    --ignore-table fleshlig_magento.salesrule_website \
     --ignore-table fleshlig_magento.s_customer_address_entity \
     --ignore-table fleshlig_magento.s_customer_address_entity_datetime \
     --ignore-table fleshlig_magento.s_customer_address_entity_decimal \
@@ -341,5 +326,17 @@ mysqldump -ufleshlig_magento -pr8R7Q7mBgeYS fleshlig_magento -t  \
     --ignore-table fleshlig_magento.s_wishlist \
     --ignore-table fleshlig_magento.s_wishlist_item \
     --ignore-table fleshlig_magento.s_wishlist_item_option \
+    --ignore_table fleshlig_magento.customer_address_entity \
+    --ignore_table fleshlig_magento.customer_address_entity_datetime \
+    --ignore_table fleshlig_magento.customer_address_entity_decimal \
+    --ignore_table fleshlig_magento.customer_address_entity_int \
+    --ignore_table fleshlig_magento.customer_address_entity_text \
+    --ignore_table fleshlig_magento.customer_address_entity_varchar \
+    --ignore_table fleshlig_magento.customer_entity \
+    --ignore_table fleshlig_magento.customer_entity_decimal \
+    --ignore_table fleshlig_magento.customer_entity_int \
+    --ignore_table fleshlig_magento.customer_entity_text \
+    --ignore_table fleshlig_magento.customer_entity_varchar \
+
     >> fli.magento.dev.dump.sql
 gzip fli.magento.dev.dump.sql
